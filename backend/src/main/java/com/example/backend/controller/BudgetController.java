b2172aca5157a5ece0eee9dca47975eede8d991d
Apekshit Gangarde <apekshitgangarde02@gmail.com>
2025-07-24 22:43:06 +0530

    added the comment

--- backend/src/main/java/com/example/backend/controller/BudgetController.java
+++ backend/src/main/java/com/example/backend/controller/BudgetController.java
@@ -18,6 +18,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api/budgets")
 public class BudgetController {
+//    this is out controller
     @Autowired
     private BudgetRepository budgetRepository;
     @Autowired
